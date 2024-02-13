a5d78a6e7a8ad11d7ba884d2e7478acd43447fbf
drp_3 <drp_3@DESKTOP-T87EQCP>
2024-02-12 23:03:43 -0600

    Agregar archivos de proyecto.

---
+++ unit 1/Program.cs
@@ -0,0 +1,15 @@
+﻿int num, aux, centena, dec, uni;
+int uni2, dec2, aux2;
+string linea;
+Console.WriteLine("Enter your three-digit number");
+linea = Console.ReadLine();
+num = int.Parse(linea);
+centena = num / 100;
+dec = num % 100;
+dec2 = dec / 10;
+uni2 = dec % 10;
+aux2 = (uni2 * 10) + dec2;
+aux = (aux2 * 10) + centena;
+Console.WriteLine("The inverted three-digit number is : " + aux);
+
+Console.ReadKey();
